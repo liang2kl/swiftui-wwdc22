d459503049ef52b6d8c978c3f4748f113d5179e5
梁业升 <liang2kl@outlook.com>
2022-06-15 22:16:58 +0800

    Initial Commit

---
+++ WWDC22/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WWDC22
+//
+//  Created by liang2kl on 2022/6/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WWDC22/WWDC22App.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC22App.swift
+//  WWDC22
+//
+//  Created by liang2kl on 2022/6/15.
+//
+
+import SwiftUI
+
+@main
+struct WWDC22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
